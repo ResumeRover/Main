242b2fdea3042fae18c278a9bdf103ac3038be26
Ravindi Gunarathna <36957677+Ravindi123@users.noreply.github.com>
2025-05-11 01:19:30 +0530

    Update API base URL to use REACT_APP_API_URL2 for PredictiveModel component

--- System Frontend/frontend/src/components/group12/PredictiveModel.jsx
+++ System Frontend/frontend/src/components/group12/PredictiveModel.jsx
@@ -9,7 +9,7 @@ import Plot from 'react-plotly.js';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, 
+  baseURL: process.env.REACT_APP_API_URL2, 
 });
 
 
